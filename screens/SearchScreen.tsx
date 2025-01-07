1ed7ea3c72aa192e92cde0ad597b1e4216f2882d
Omar Faruk <omarbg.bd@gmail.com>
2025-01-07 21:04:03 +0600

    single card for search page

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -6,57 +6,149 @@ import {
 	FlatList,
 	Text,
 	View,
+	Image,
 } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 const SearchScreen = () => {
-	const [query, setQuery] = React.useState("");
-	const data = ["Pasta", "Pizza", "Salad", "Soup", "Cake", "Smoothie"];
-
-	const filteredData = data.filter((item) =>
-		item.toLowerCase().includes(query.toLowerCase())
-	);
-
 	return (
-		<SafeAreaView style={styles.container}>
-			<TextInput
-				style={styles.input}
-				placeholder="Search recipes..."
-				value={query}
-				onChangeText={setQuery}
-			/>
-			<FlatList
-				data={filteredData}
-				keyExtractor={(item, index) => index.toString()}
-				renderItem={({ item }) => (
-					<View style={styles.item}>
-						<Text style={styles.text}>{item}</Text>
+		<SafeAreaView style={styles.safeArea}>
+			<View style={styles.Searchcontainer}>
+				<Ionicons name="search" size={20} color="#aaa" style={styles.icon} />
+				<TextInput
+					style={styles.input}
+					placeholder="Search any recipes"
+					placeholderTextColor="#aaa"
+				/>
+			</View>
+
+			{/* search result contents */}
+			<View style={styles.resultContainer}>
+				{/* single card design start*/}
+				<View style={styles.singleItemCard}>
+					<View>
+						<Image
+							source={require("../assets/images/profile.jpeg")} // Replace with the path to your local image
+							style={styles.singleItemCardImage}
+						/>
 					</View>
-				)}
-			/>
+					<View style={styles.singleItemCardContent}>
+						<Text style={styles.singleItemCardContentTitle}>Awesome Title</Text>
+						<Text style={styles.singleItemCardContentDescription}>
+							Lorem Ipsum Doler
+						</Text>
+					</View>
+				</View>
+
+				<View style={styles.singleItemCard}>
+					<View>
+						<Image
+							source={require("../assets/images/profile.jpeg")} // Replace with the path to your local image
+							style={styles.singleItemCardImage}
+						/>
+					</View>
+					<View style={styles.singleItemCardContent}>
+						<Text style={styles.singleItemCardContentTitle}>Awesome Title</Text>
+						<Text style={styles.singleItemCardContentDescription}>
+							Lorem Ipsum Doler
+						</Text>
+					</View>
+				</View>
+
+				<View style={styles.singleItemCard}>
+					<View>
+						<Image
+							source={require("../assets/images/profile.jpeg")} // Replace with the path to your local image
+							style={styles.singleItemCardImage}
+						/>
+					</View>
+					<View style={styles.singleItemCardContent}>
+						<Text style={styles.singleItemCardContentTitle}>Awesome Title</Text>
+						<Text style={styles.singleItemCardContentDescription}>
+							Lorem Ipsum Doler
+						</Text>
+					</View>
+				</View>
+
+				<View style={styles.singleItemCard}>
+					<View>
+						<Image
+							source={require("../assets/images/profile.jpeg")} // Replace with the path to your local image
+							style={styles.singleItemCardImage}
+						/>
+					</View>
+					<View style={styles.singleItemCardContent}>
+						<Text style={styles.singleItemCardContentTitle}>Awesome Title</Text>
+						<Text style={styles.singleItemCardContentDescription}>
+							Lorem Ipsum Doler
+						</Text>
+					</View>
+				</View>
+				{/* single card design end*/}
+			</View>
 		</SafeAreaView>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {
+	safeArea: {
 		flex: 1,
 		backgroundColor: "#fff",
-		padding: 10,
 	},
-	input: {
+	Searchcontainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		backgroundColor: "#fff",
+		borderRadius: 25,
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		marginHorizontal: 20,
 		borderWidth: 1,
 		borderColor: "#ccc",
-		borderRadius: 5,
-		padding: 10,
-		marginBottom: 10,
+		minHeight: 50,
+		marginTop: 30,
 	},
-	item: {
-		padding: 15,
-		borderBottomWidth: 1,
-		borderBottomColor: "#ddd",
+	icon: {
+		marginRight: 10,
 	},
-	text: {
+	input: {
+		flex: 1,
 		fontSize: 16,
+		color: "#000",
+	},
+
+	// search result content
+	resultContainer: {
+		marginTop: 30,
+	},
+	singleItemCard: {
+		display: "flex",
+		flexDirection: "row",
+		borderColor: "#ccc",
+		borderWidth: 1,
+		padding: 10,
+		margin: 10,
+		marginHorizontal: 20,
+		borderRadius: 10,
+	},
+	singleItemCardImage: {
+		width: 50,
+		height: 50,
+		borderRadius: 100,
+	},
+	singleItemCardContent: {
+		marginLeft: 20,
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	singleItemCardContentTitle: {
+		color: "#333",
+		fontSize: 18,
+		fontWeight: "bold",
+		marginBottom: 5,
+	},
+	singleItemCardContentDescription: {
+		color: "#444",
 	},
 });
 
