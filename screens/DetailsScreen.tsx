b792ada0c9d9e63730c8bfc3c9f84c46c6b1b12a
Omar Faruk <omarbg.bd@gmail.com>
2025-01-08 20:38:32 +0600

    remove safe area from details screen

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -7,8 +7,6 @@ import Animated, {
 	useAnimatedStyle,
 	useAnimatedScrollHandler,
 } from "react-native-reanimated";
-
-import { SafeAreaView } from "react-native-safe-area-context";
 import { MaterialIcons } from "@expo/vector-icons";
 import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
